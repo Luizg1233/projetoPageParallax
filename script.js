09e6998443fe4a986b2c8c7d2d9edb73a8f589e9
Luiz Gabriel <luizgabriel1233@gmail.com>
2022-03-29 05:41:18 -0300

    1.0

---
+++ script.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('.area-1', {
+    rotate:{x:0, y:80, z:0},
+    origin: 'bottom',
+    easing: 'ease-in',
+    
+});
+sr.reveal('.area-2', {
+    rotate:{x:0, y:80, z:0},
+    origin: 'bottom',
+    easing: 'ease-in',
+    
+});
+sr.reveal('.area-3', {
+    rotate:{x:0, y:80, z:0},
+    origin: 'bottom',
+    easing: 'ease-in',
+    
+});
